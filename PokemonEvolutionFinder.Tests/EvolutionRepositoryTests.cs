9de2f8091b647be47d36300c5f4c91dc78c2f74a
kj <kj-birk@outlook.com>
2023-01-23 09:26:22 +0100

    add pokemon repository

---
+++ PokemonEvolutionFinder.Tests/EvolutionRepositoryTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Json;
+using PokemonEvolutionFinder.Models.Pokemon;
+
+namespace PokemonEvolutionFinder.Tests;
+
+public class EvolutionRepositoryTests
+{
+    [Fact]
+    public async void RepositoryCanGetSpeciesUrlByPokemonName()
+    {
+        string name = "charmander";
+        var repository = new EvolutionRepository(new HttpClient());
+
+        var pokemon = await repository.GetByName(name);
+        //var evolutions = await client.GetFromJsonAsync<SpeciesDto>(pokemon.Species.url);
+        
+        Assert.Equal("https://pokeapi.co/api/v2/pokemon-species/4/", pokemon.species.url);
+    }
+}
---
+++ PokemonEvolutionFinder/Models/Pokemon/PokemonRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace PokemonEvolutionFinder.Models.Pokemon;
+
+public class EvolutionRepository
+{
+    private HttpClient _client;
+
+    public EvolutionRepository(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<Root> GetByName(string name)
+    {
+        return await _client.GetFromJsonAsync<Root>($"https://pokeapi.co/api/v2/pokemon/{name}");
+    }
+}
