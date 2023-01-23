a6daf728094ca9842d52d31f127c8c2358e641e4
kj <kj-birk@outlook.com>
2023-01-23 10:40:06 +0100

    update controller tests (to fail)

---
+++ PokemonEvolutionFinder.Tests/EvolutionFinderTests.cs
@@ -0,0 +1,29 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Win32;
+using PokemonEvolutionFinder.Controllers;
+using PokemonEvolutionFinder.Models;
+using Xunit;
+
+namespace PokemonEvolutionFinder.Tests;
+
+public class EvolutionFinderTests
+{
+    public static IEnumerable<object> CollectionOfEvolutionChains()
+    {
+        yield return new object[] { new List<string> { "charmander", "charmeleon", "charizard" }, "charmander" };
+        yield return new object[] { new List<string> { "squirtle", "wartortle", "blastoise" }, "squirtle" };
+    }
+    
+    [Theory]
+    [MemberData(nameof(CollectionOfEvolutionChains))]
+    public async void ControllerReturnsEvolutionChainGivenPokemonName(List<string> expected, string name)
+    {
+        var controller = new EvolutionFinderController();
+
+        var result = await controller.GetAsync(name);
+        
+        Assert.Equal(result, expected);
+    }
+}
